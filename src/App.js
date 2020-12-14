e68ebc392accf717c5e538fc5f2645b0d9510fee
matthewvero <mattavero@hotmail.com>
2020-12-14 15:49:54 +0000

    import bug fix

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ import ProfilePage from "./pages/profilepage/profilepage";
 import MainMenu from "./components/mainmenu/main-menu.component";
 import { getUserImages } from "./firebase.utils";
 import { SignedIn } from "./events/auth-events";
-import Tilt from "./components/tilt/tilt.component";
 
 function App() {
 	const [darkMode, setDarkmode] = useState(
